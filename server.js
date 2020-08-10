894ff5c93131fead2aa8caa15ca52de426b7386d
sjothishunmugam <sjothishunmugam@deakin.edu.au>
2020-08-10 20:34:00 +1000

    Started the assignment

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+// const port = 3000;
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/test", function (request, response) {
+  var user_name = request.query.user_name;
+  response.end("Hello " + user_name + "!");
+});
+
+app.listen(port);
+console.log("Listening on port ", port);
+
+require("cf-deployment-tracker-client").track();
