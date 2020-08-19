31c2ea4d8bcfcc6042b14b7ff6791ea50def760c
sjothishunmugam <sjothishunmugam@deakin.edu.au>
2020-08-19 20:09:35 +1000

    Completed Spotify assignment

--- public/env.js
+++ public/env.js
@@ -1,18 +1,101 @@
-const testButtonFunction=()=>{
-  alert('Thank you for clicking')
-}
-
-
 $(document).ready(function(){
   console.log('Ready')
+  let idnumber;
+  let currentSongNumber = 0;
+  let songs = null;
+  //DB calling funciton
+  const dbcallingfunction=(idnumber)=>{
+
+    let data = {
+      id : idnumber
+    }
   
-  //bind the button
-  $('#testButton').click(testButtonFunction)
+    $.get('/messages',data, function(messages){
+      $('#messageindox').empty();
+      messages.forEach((message)=>{
+        $('#messageindox').append('<div class="row">'+message.message+'</div>')
+      })
+      
+    })
 
-  //test get call
-  $.get('/test?user_name="Fantastic User"',(result)=>{
-    console.log(result)
-  })
+  }
+  dbcallingfunction(1);
+  songs = ["song-a","song-b","song-c","song-d","song-e"]
 
+  //click function for the Intention song
+    $("#intentionImage").click(function () {
+      $("#sheet").css("display", "none");
+      // alert('Thank you for clicking button 1')
+      idnumber= 1;
+      currentSongNumber = idnumber-1;
+
+    dbcallingfunction(idnumber); 
+    document.getElementById("my-audio").setAttribute("src", `assets/${songs[idnumber-1]}.mp3`);
+    });
+
+  //click function for the Lovemelikeyoudo song
+  // $(function () {
+    $("#lovemelikeyoudoImage").click(function () {
+      $("#sheet").css("display", "none");
+      // alert('Thank you for clicking button 2')
+      idnumber= 2;
+      currentSongNumber = idnumber-1;
+
+      dbcallingfunction(idnumber);
+      document.getElementById("my-audio")
+      .setAttribute("src", `assets/${songs[idnumber-1]}.mp3`);
+    });
+
+//click function for blankspace song
+  $("#blankspaceImage").click(function () {
+    $("#sheet").css("display", "none");
+    // alert('Thank you for clicking button 2')
+    idnumber= 3;
+    currentSongNumber = idnumber-1;
+    
+    dbcallingfunction(idnumber);
+    document.getElementById("my-audio")
+    .setAttribute("src", `assets/${songs[idnumber-1]}.mp3`);
+  });
+
+  //click function for girlslikeyouImage song
+  $("#girlslikeyouImage").click(function () {
+    $("#sheet").css("display", "none");
+    // alert('Thank you for clicking button 2')
+    idnumber= 4;
+    currentSongNumber = idnumber-1;
+    
+    dbcallingfunction(idnumber);
+    document.getElementById("my-audio")
+    .setAttribute("src", `assets/${songs[idnumber-1]}.mp3`);
+  });
+
+  //click function for memoriesImage song
+  $("#memoriesImage").click(function () {
+    $("#sheet").css("display", "none");
+    // alert('Thank you for clicking button 2')
+    idnumber= 5;
+    currentSongNumber = idnumber-1;
+    
+    dbcallingfunction(idnumber);
+    document.getElementById("my-audio")
+    .setAttribute("src", `assets/${songs[idnumber-1]}.mp3`);
+  });
+
+
+const playNextSong=()=>{
+    // alert('Playling next song')
+    if (currentSongNumber >= songs.length - 1) {
+        currentSongNumber = 0;
+    } else {
+        currentSongNumber = currentSongNumber + 1;
+    }
+    dbcallingfunction(currentSongNumber+1); 
+    document
+    .getElementById("my-audio")
+    .setAttribute("src", `assets/${songs[currentSongNumber]}.mp3`);
+  }
+
+$('#nextButton').click(playNextSong)
 
 })
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// "start": "node server.js"
+const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 const app = express();
 // const port = 3000;
@@ -10,6 +12,33 @@ app.get("/test", function (request, response) {
   var user_name = request.query.user_name;
   response.end("Hello " + user_name + "!");
 });
+
+//Server end point for getting message
+app.get('/messages', function(req,res){
+  retreiveMessages(req,res);
+
+});
+
+let collectionMessage;
+const uri = "mongodb+srv://assignment1:SIT725@assignment1.5cgvp.mongodb.net/spotifydb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+
+client.connect(err => {
+  collectionMessage = client.db("spotifyDB").collection("spotify");
+  // perform actions on the collection object
+  // collectionMessage.insert({_id:5,message:'Memories is a song by American pop rock band Maroon 5. Released on September 20, 2019'})
+  // client.close();
+});
+
+//retreivemessage funtion for DB
+const retreiveMessages=(req,res)=>{
+  let id = parseInt(req.query.id);
+  collectionMessage.find({_id : id }).toArray(function(err,result){   
+    if (err) throw err;
+    res.send(result)
+  })
+}
 
 app.listen(port);
 console.log("Listening on port ", port);
